665a91d96794b84111dd00a5fdd15235e910cea5
Ramya <emailtoramya@gmail.com>
2015-10-26 19:11:24 -0400

    Comm3 - Refined further by 'Removing all even numbers first and then looping'

--- SieveOfEratosthenes/SieveOfEratosthenes/Program.cs
+++ SieveOfEratosthenes/SieveOfEratosthenes/Program.cs
@@ -18,7 +18,9 @@ namespace SieveOfEratosthenes
             for (int i = 2; i <= input; i++)
                 inputList.Add(i);
 
-            var output = SOE_Impl(inputList, input);
+            //var output = SOE_Impl1(inputList, input);
+            //var output = SOE_Impl2(inputList, input);
+            var output = SOE_Impl3(inputList, input);
 
             Console.WriteLine("Prime numbers are....");
             foreach (var value in output)
@@ -36,7 +38,7 @@ namespace SieveOfEratosthenes
         /// <param name="inputList"></param>
         /// <param name="maxValueInList"></param>
         /// <returns></returns>
-        public static List<int> SOE_Impl(List<int> inputList, int maxValueInList)
+        public static List<int> SOE_Impl1(List<int> inputList, int maxValueInList)
         {
             var sqRootOfInput = (int)Math.Sqrt(maxValueInList);
             var markedToDelete = new List<int>();
@@ -58,5 +60,73 @@ namespace SieveOfEratosthenes
             inputList.RemoveAll(x => markedToDelete.Contains(x));
             return inputList;
         }
+
+        /// <summary>
+        /// to refine further, loop only until n/p
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <param name="maxValueInList"></param>
+        /// <returns></returns>
+        public static List<int> SOE_Impl2(List<int> inputList, int maxValueInList)
+        {
+            var sqRootOfInput = (int)Math.Sqrt(maxValueInList);
+            var markedToDelete = new List<int>();
+
+            foreach (var currNum in inputList)
+            {
+                if (currNum > sqRootOfInput)
+                    break;
+
+                var exitAfter = (maxValueInList / currNum); // will further reduce the number of loops 
+
+                if (markedToDelete.Contains(currNum))
+                    continue;
+
+                int incCounter = currNum;
+                while (incCounter <= exitAfter)
+                {
+                    markedToDelete.Add(currNum * incCounter);
+                    incCounter++;
+                }
+            }
+            inputList.RemoveAll(x => markedToDelete.Contains(x));
+            return inputList;
+        }
+
+        /// <summary>
+        /// to refine further, removed all even numbers (because they are multiples of 2) except 2 and then continued like first implementation
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <param name="maxValueInList"></param>
+        /// <returns></returns>
+        public static List<int> SOE_Impl3(List<int> inputList, int maxValueInList)
+        {
+            var sqRootOfInput = (int)Math.Sqrt(maxValueInList);
+
+            //Remove all Even numbers except 2
+            inputList.RemoveAll(x => x != 2 && x % 2 == 0);
+
+            var markedToDelete = new List<int>();
+
+            foreach (var currNum in inputList)
+            {
+                if (currNum == 2)
+                    continue;
+                if (currNum > sqRootOfInput)
+                    break;
+                if (markedToDelete.Contains(currNum))
+                    continue;
+
+                int incCounter = currNum;
+                while (incCounter <= maxValueInList)
+                {
+                    markedToDelete.Add(currNum + incCounter);
+                    incCounter = incCounter + currNum;
+                }
+            }
+            inputList.RemoveAll(x => markedToDelete.Contains(x));
+            return inputList;
+        }
+
     }
 }
